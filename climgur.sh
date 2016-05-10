4f5bf1386c20333a29aea66a44e88055d25bda23
cesar <cesar@poa.nyc>
2016-05-10 13:29:15 -0400

    setting getopts

--- climgur.sh
+++ climgur.sh
@@ -4,13 +4,13 @@ set -e
 set -o pipefail
 clear
 readonly NAME=$(basename $0)
-readonly APIKEY=""
+readonly CLIENT_ID=""
 readonly VER="0.01"
 
 # temp file and trap statement - trap for clean end
 case "$(uname 2>/dev/null)" in
-    'Linux') local TMP_FILE=$(mktemp --tmpdir img_$$-XXXXXX.png) ;;
-    'Darwin') local TMP_FILE=$(mktemp img_$$-XXXXXX.png) ;;
+    'Linux') local TMP_FILE=$(mktemp --tmpdir img_$$-XXXX.png) ;;
+    'Darwin') local TMP_FILE=$(mktemp img_$$-XXXX.png) ;;
 esac
 
 trap 'printf "${NAME}: Quitting.\n\n" 1>&2 ; \
@@ -24,12 +24,27 @@ trap 'printf "${NAME}: Quitting.\n\n" 1>&2 ; \
 [ -z $(which curl 2>/dev/null) ] &&\
     { printf "%s\n" "curl not found"; exit 1; }
 
+while getopts ":hsu:" OPT; do
+    case "${OPT}" in
+        'h'|'-h') usage ;;
+        's'|'-s') screenshot ;;
+        'u'|'-u') file upload ;;
+        *) ;;
+    esac
+done
+shift $((OPTIND-1))
+
+
+
 function screenshot()
 {
+    # $(which scrot) -z "${_SC_OPT}" ${TMP_FILE} >/dev/null 2>&1
+    $(which scrot) -z "${_SC_OPT}" ${TMP_FILE} >/dev/null 2>&1
+}
 
-if [ ${NFILE} -eq 1 ]; then
-        $(which scrot) -z "${_SC_OPT}" ${TMP_FILE} >/dev/null 2>&1
-fi
 
-
+function upload()
+{
+curl -sH "Authorization: Client-ID ${CLIENT_ID}" -F "image=@${TMP_FILE}" \
+    "https://api.imgur.com/3/upload"
 }
