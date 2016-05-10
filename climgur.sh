0f2dbefcb5dfa676bd55f5637431654215dd5f7a
cesar <cesar@poa.nyc>
2016-05-10 02:24:31 -0400

    meh

--- climgur.sh
+++ climgur.sh
@@ -5,48 +5,25 @@ set -o pipefail
 clear
 readonly NAME=$(basename $0)
 readonly APIKEY=""
+readonly VER="0.01"
 
-version()
+# temp file and trap statement - trap for clean end
+case "$(uname 2>/dev/null)" in
+    'Linux') local TMP_FILE=$(mktemp --tmpdir img_$$-XXXXXX.png) ;;
+    'Darwin') local TMP_FILE=$(mktemp img_$$-XXXXXX.png) ;;
+esac
+
+trap 'printf "${NAME}: Quitting.\n\n" 1>&2 ; \
+    rm -rf ${TMP_FILE} ; exit 1' 0 1 2 3 9 15
+
+# check if scrot exists
+[ -z $(which scrot 2>/dev/null) ] &&\
+    { printf "%s\n" "scrot not found"; exit 1; }
+
+# check if curl exists
+[ -z $(which curl 2>/dev/null) ] &&\
+    { printf "%s\n" "curl not found"; exit 1; }
+
+function screenshot()
 {
-    local VER="0.01"
-    cat <<EOL
-    ${NAME} version ${VER}
-    Copyright (C) 2016 cesar@pissedoffadmins.com
-    This program comes with ABSOLUTELY NO WARRANTY.
-    This is free software, and you are welcome to redistribute it.
 
-    EOL
-}
-
-descrip()
-{
-    cat <<EOL
-
-    EOL
-}
-
-usage()
-{
-    cat <<EOL
-
-    EOL
-}
-
-function main()
-{
-    # temp file and trap statement - trap for clean end
-    case "$(uname 2>/dev/null)" in
-        'Linux') TMP_FILE=$(mktemp --tmpdir img_$$-XXXXXX.png) ;;
-        'Darwin') TMP_FILE=$(mktemp img_$$-XXXXXX.png) ;;
-    esac
-    trap 'printf "${NAME}: Quitting.\n\n" 1>&2 ; \
-        rm -rf ${TMP_FILE} ; exit 1' 0 1 2 3 9 15
-
-    # check if scrot exists
-    [ -z $(which scrot 2>/dev/null) ] &&\
-        { printf "%s\n" "scrot not found"; exit 1; }
-
-        # check if curl exists
-        [ -z $(which curl 2>/dev/null) ] &&\
-            { printf "%s\n" "curl not found"; exit 1; }
-        }
