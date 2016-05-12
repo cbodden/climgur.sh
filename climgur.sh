cf94c67cede383ae9a07eb3f7720f85bf6b13ae3
cesar <cesar@poa.nyc>
2016-05-12 03:46:27 -0400

    working the uploads and file check

--- climgur.sh
+++ climgur.sh
@@ -14,25 +14,31 @@ esac
 trap 'rm -rf ${TMP_FILE} ; exit 1' 0 1 2 3 9 15
 
 # check if scrot exists
-[ -z $(which scrot 2>/dev/null) ] &&\
-    { printf "%s\n" "scrot not found"; exit 1; }
+[ -z $(which scrot 2>/dev/null) ] \
+    && { printf "%s\n" "scrot not found"; exit 1; }
 
 # check if curl exists
-[ -z $(which curl 2>/dev/null) ] &&\
-    { printf "%s\n" "curl not found"; exit 1; }
+[ -z $(which curl 2>/dev/null) ] \
+    && { printf "%s\n" "curl not found"; exit 1; }
 
 function image()
 {
     case "${IMAGE}" in
         's'|'ss'|'screenshot')
-            #$(which scrot) -z "${_SC_OPT}" ${TMP_FILE} >/dev/null 2>&1
             $(which scrot) -z ${TMP_FILE} >/dev/null 2>&1
             curl -sH "Authorization: Client-ID ${CLIENT_ID}" \
                 -F "image=@${TMP_FILE}" "https://api.imgur.com/3/upload" |\
                 python -m json.tool |\
                 sed -e 's/^ *//g' -e '/{/d' -e '/}/d'
         ;;
-        'u'|'upload') ;;
+        'u'|'upload') printf "\n${IMAGE}\n"
+            printf "Path to file ? (full path) : " ; read _FILE_PATH
+            if [ $(file ${_FILE_PATH} | grep -c "image data") -ne 0 ]; then
+                echo "file is good"
+            else
+                echo "check file name"
+            fi
+        ;;
         *) printf "\nOptions\n\n" ;;
     esac
 }
@@ -40,14 +46,6 @@ function image()
 function usage()
 {
     printf "\ntesting\n\n"
-}
-
-function upload()
-{
-curl -sH \
-    "Authorization: Client-ID ${CLIENT_ID}" \
-    -F "image=@${TMP_FILE}" \
-    "https://api.imgur.com/3/upload"
 }
 
 function account_info()
@@ -58,14 +56,14 @@ curl -sH \
     python -m json.tool
 }
 
-while getopts "ahi:su:" OPT; do
+while getopts "ahi:s" OPT; do
     case "${OPT}" in
         a) account_info ;;
         h) usage ;;
         i) IMAGE=$OPTARG
             image ;;
-        s) screenshot ;;
-        u) file upload ;;
+        s) IMAGE="screenshot"
+            image ;;
     esac
 done
 [ ${OPTIND} -eq 1 ] && { usage ; }
